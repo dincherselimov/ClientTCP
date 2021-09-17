00851c40c33f1b428555958434e4e32ec49c3583
dincher <80629557+dincherselimov@users.noreply.github.com>
2021-09-17 14:36:27 +0300

    Add files via upload

--- Client/src/com/client/ClientTCP.java
+++ Client/src/com/client/ClientTCP.java
@@ -1,22 +1,29 @@
 package com.client;
+import Config.Config;
 import java.io.*;
 import java.net.*;
+/**
+ *
+ */
 public class ClientTCP {
     public static void main(String[] args) throws IOException {
 
+
         try {
             //connect to the server
-            Socket socket = new Socket("localhost", 80);
+            Socket socket = new Socket("localhost",Config.getInstance().getPort());
 
             DataOutputStream outputStream =new DataOutputStream(socket.getOutputStream());
             outputStream.writeUTF("Hello World");
             outputStream.flush();
             outputStream.close();
 
+            //closing the socket
             socket.close();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+             catch (Exception e) {
+                 e.printStackTrace();
         }
     }
 
